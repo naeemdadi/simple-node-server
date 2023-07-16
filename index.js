6b3c0afa495c3678aa36b09aff77f6aeb2decd94
naeemdadi <naeemdadi85@gmail.com>
2023-07-16 18:38:24 +0530

    feat: get products url update

--- index.js
+++ index.js
@@ -13,44 +13,57 @@ app.use(cors({ credentials: true, origin: true }));
 axios.defaults.baseURL = process.env.URL;
 
 app.get("/getToken", async (req, res) => {
-  // Send a POST request
-  const response = await axios.post("/generate-token", req.data, {
-    headers: {
-      "Api-key": process.env.apiKey,
-    },
-  });
+  try {
+    // Send a POST request
+    const response = await axios.post("/generate-token", req.data, {
+      headers: {
+        "Api-key": process.env.apiKey,
+      },
+    });
 
-  const { data } = response;
+    const { data } = response;
 
-  if (!data.status) {
-    res.send(data.errors || "Failed to generate token");
-  } else {
-    res.send(data.data);
+    if (!data.status) {
+      res.send(data.errors || "Failed to generate token");
+    } else {
+      res.send(data.data);
+    }
+  } catch (err) {
+    throw new Error(err);
   }
 });
 
 app.post("/getProducts", async (req, res) => {
   const { authToken } = req.body;
-  // Send a POST request
-  const response = await axios.post(
-    "/plan-list",
-    {
-      session_id: process.env.SESSION_ID,
-    },
-    {
-      headers: {
-        "Api-key": process.env.apiKey,
-        "Auth-token": authToken,
+  if (!authToken) {
+    res.status(404).send({ message: "authToken is missing!" });
+  }
+  console.log({ authToken });
+  try {
+    // Send a POST request
+    const response = await axios.post(
+      "/plan-list",
+      {
+        session_id: process.env.SESSION_ID,
       },
+      {
+        headers: {
+          "Api-key": process.env.apiKey,
+          "Auth-token": authToken,
+        },
+      }
+    );
+
+    const { data } = response;
+
+    if (!data.status) {
+      res.send(data.errors || "Failed to get Products");
+    } else {
+      res.send(data.data);
     }
-  );
-
-  const { data } = response;
-
-  if (!data.status) {
-    res.send(data.errors || "Failed to generate token");
-  } else {
-    res.send(data.data);
+  } catch (err) {
+    // console.log({ err });
+    res.send(err);
   }
 });
 
