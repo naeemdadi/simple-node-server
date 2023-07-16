b9e3c9ef76998d911c2948df99b2793466b7774f
naeemdadi <naeemdadi85@gmail.com>
2023-07-16 18:16:53 +0530

    feat: get products url

--- index.js
+++ index.js
@@ -10,13 +10,40 @@ const port = process.env.PORT || 9000;
 app.use(express.json());
 app.use(cors({ credentials: true, origin: true }));
 
+axios.defaults.baseURL = process.env.URL;
+
 app.get("/getToken", async (req, res) => {
   // Send a POST request
-  const response = await axios.post(process.env.URL, req.data, {
+  const response = await axios.post("/generate-token", req.data, {
     headers: {
       "Api-key": process.env.apiKey,
     },
   });
+
+  const { data } = response;
+
+  if (!data.status) {
+    res.send(data.errors || "Failed to generate token");
+  } else {
+    res.send(data.data);
+  }
+});
+
+app.post("/getProducts", async (req, res) => {
+  const { authToken } = req.body;
+  // Send a POST request
+  const response = await axios.post(
+    "/plan-list",
+    {
+      session_id: process.env.SESSION_ID,
+    },
+    {
+      headers: {
+        "Api-key": process.env.apiKey,
+        "Auth-token": authToken,
+      },
+    }
+  );
 
   const { data } = response;
 
